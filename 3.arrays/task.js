13c6df66b4538de1357e0e97cb0bb7e586bc51a9
Anna <akhrolenko85@gmail.com>
2022-10-17 21:47:16 +0300

    ДЗ 3 массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
