a5eb4cded90c89c32783cd62ccaa9bfe268b36d2
Anna <akhrolenko85@gmail.com>
2022-11-09 23:46:22 +0300

    дз декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,32 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];  
+    function wrapper(...args) {
+      const hash = args.join(',');
+      let idx = cache.findIndex((item) => item.hash == hash);
+      if (idx !== -1) {
+        console.log("Из кэша: " + cache[idx].result);
+        return "Из кэша: " + cache[idx].result;
+      }
+  
+      let result = func(...args);
+      cache.push({hash, result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer = null;  
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+    return wrapper;
 }
